d74bf943e1940732f09c12e63b6fb3c00d0bab4a
Milos Zivlak <zivlakmilos@gmail.com>
2017-03-19 15:31:18 +0100

    Player don't extends sprite anymore (use spritesheet instad)

--- js/player.js
+++ js/player.js
@@ -1,12 +1,56 @@
-class Player extends Sprite {
+class Player {
     constructor(x, y, width, height, sprite) {
-        super(x, y, width, height, sprite);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.sprite = sprite;
 
         this.velX = 0.0;
         this.velY = 0.0;
     }
 
+    get getX() {
+        return this.x;
+    }
+
+    set setX(x) {
+        this.x = x;
+    }
+
+    get getY() {
+        return this.y;
+    }
+
+    set setY(y) {
+        this.y = y;
+    }
+
+    get getWidth() {
+        return this.width;
+    }
+
+    set setWidth(width) {
+        this.width = width;
+    }
+
+    get getHeight() {
+        return this.height;
+    }
+
+    set setHeight(height) {
+        this.height = height;
+    }
+
     update(canvas, tick) {
-        super.render(canvas);
+        this.render(canvas);
+    }
+
+    render(canvas) {
+        var ctx = canvas.getContext("2d");
+
+        ctx.drawImage(this.sprite,
+                      this.sprite.width / 3, 0, this.sprite.width / 3, this.sprite.height,
+                      this.x, this.y, this.width, this.height);
     }
 }
