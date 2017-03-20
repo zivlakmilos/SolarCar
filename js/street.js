00301da29fe8120ca8f7daf825b21039ec591a06
Milos Zivlak <zivlakmilos@gmail.com>
2017-03-20 21:34:19 +0100

    Added basic street (working)

--- js/street.js
+++ js/street.js
@@ -1,27 +1,21 @@
-class Segment {
-    constructor(y, height, color) {
-        this.y1 = y;
-        this.height = height;
-        this.y2 = this.y1 + this.y1 * this.height / 100.0;
+class Line {
+    constructor(z, color) {
+        this.ddz = 4;
+        this.dz = 20;
+        this.z = z;
+        this.roadLength = 250.0;
+
         this.color = color;
-
-        this.speed = 100.0;
     }
 
-    get getY1() {
-        return this.y1;
-    }
-
-    get getY2() {
-        return this.y2;
+    get getZ() {
+        return this.z;
     }
 
     update(canvas, tick) {
-        this.y1 += this.speed * tick;
-        this.x1 = 100.0 * (canvas.height - this.y1) / (canvas.height / 3.0);
-
-        this.y2 = this.y1 + this.y1 * this.height / 250.0;
-        this.x2 = 100.0 * (canvas.height - this.y2) / (canvas.height / 3.0);
+        this.dz -= this.ddz * tick;
+        //this.dz = 4;
+        this.z -= this.dz;
 
         this.render(canvas);
     }
@@ -29,25 +23,64 @@ class Segment {
     render(canvas) {
         var ctx = canvas.getContext("2d");
 
+        if(this.z < 0)
+            return;
+
+        var scale1 = 1.0 / this.z;
+        var y1 = canvas.height * scale1;
+        y1 = (canvas.height / 2) - (canvas.height / 2) * y1;
+        var x1 = (canvas.width + 25.0) * scale1;
+        x1 = (canvas.width / 2) + (canvas.width / 2) * x1;
+
+        var scale2 = 1.0 / (this.z + this.roadLength);
+        var y2 = canvas.height * scale2;
+        y2 = (canvas.height / 2) - (canvas.height / 2) * y2;
+        var x2 = (canvas.width + 25.0) * scale2;
+        x2 = (canvas.width / 2) + (canvas.width / 2) * x2;
+
+        y1 = canvas.height - y1;
+        y2 = canvas.height - y2;
+
         ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.moveTo(this.x1, this.y1);
-        ctx.lineTo(canvas.width - this.x1, this.y1);
-        ctx.lineTo(canvas.width - this.x2, this.y2);
-        ctx.lineTo(this.x2, this.y2);
-        ctx.lineTo(this.x1, this.y1);
+        if(this.color > 0)
+            ctx.fillStyle = "#FF0000";
+        else
+            ctx.fillStyle = "#FFFFFF"
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(canvas.width - x1, y1);
+        ctx.lineTo(canvas.width - x2, y2);
+        ctx.lineTo(x2, y2);
+        ctx.lineTo(x1, y1);
         ctx.fill();
         ctx.closePath();
 
+        var scale1 = 1.0 / this.z;
+        var y1 = canvas.height * scale1;
+        y1 = (canvas.height / 2) - (canvas.height / 2) * y1;
+        var x1 = canvas.width * scale1;
+        x1 = (canvas.width / 2) + (canvas.width / 2) * x1;
+
+        var scale2 = 1.0 / (this.z + this.roadLength);
+        var y2 = canvas.height * scale2;
+        y2 = (canvas.height / 2) - (canvas.height / 2) * y2;
+        var x2 = canvas.width * scale2;
+        x2 = (canvas.width / 2) + (canvas.width / 2) * x2;
+
+        y1 = canvas.height - y1;
+        y2 = canvas.height - y2;
+
         ctx.beginPath();
-        ctx.fillStyle = "#CC0000";
-        ctx.moveTo(this.x1 - 25, this.y1);
-        ctx.lineTo(this.x1, this.y1);
-        ctx.lineTo(this.x2, this.y2);
-        ctx.lineTo(this.x2 - 25, this.y2);
-        ctx.lineTo(this.x1 - 25, this.y1);
+        if(this.color > 0)
+            ctx.fillStyle = "#939393";
+        else
+            ctx.fillStyle = "#858585"
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(canvas.width - x1, y1);
+        ctx.lineTo(canvas.width - x2, y2);
+        ctx.lineTo(x2, y2);
+        ctx.lineTo(x1, y1);
         ctx.fill();
-        ctx.endPath();
+        ctx.closePath();
     }
 }
 
@@ -55,33 +88,26 @@ class Street {
     constructor() {
         this.segments = [];
         this.segmentColor = 0;
-
-        //for(var i = 0; i < 10; i++) {
-            //this.segments.push(new Segment(100 * i, 100));
-        //}
-        //this.segments.push(new Segment(100, 100));
     }
 
     update(canvas, tick) {
         if(this.segments.length < 1) {
-            this.segments.push(new Segment(canvas.height / 3.0 + 25, 25, "#555555"));
-        } else if(this.segments[this.segments.length - 1].getY2 > canvas.height / 3.0 + 100) {
-            var color = "#555555";
-            if(this.segmentColor > 0) {
-                color = "#333333";
-                this.segmentColor = 0
-            } else {
+            this.segments.push(new Line(2000.0, this.segmentColor));
+            this.segmentColor++;
+        } else if(this.segments[this.segments.length - 1].getZ < 1750) {
+            this.segments.push(new Line(2000.0, this.segmentColor));
+
+            if(this.segmentColor > 0)
+                this.segmentColor = 0;
+            else
                 this.segmentColor++;
-            }
-            this.segments.push(new Segment(canvas.height / 3.0 + 50, 25, color));
         }
 
         for(var i = 0; i < this.segments.length; i++) {
             this.segments[i].update(canvas, tick);
-            if(this.segments[i].getY1 > canvas.height)
+            if(this.segments[i].getZ < 1)
                 this.segments.splice(i--, 1);
         }
-        //this.render(canvas);
     }
 
     render(canvas) {
